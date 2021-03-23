1e64c6149cf5609ccf96c39b43ceff28fa1c6193
Erhan Dolapci <EDolapci@gs1us.org>
2021-03-23 16:15:50 -0400

    Add project files.

---
+++ OverridingMethod/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverridingMethod
+{
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing Shape");
+        }
+
+    }
+}
